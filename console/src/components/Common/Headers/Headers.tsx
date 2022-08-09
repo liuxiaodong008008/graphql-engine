ba61388c4472b67504bdd866910c582d416e21b0
LiuXiaodong <31185582+liuxiaodong008008@users.noreply.github.com>
2022-08-09 23:36:05 +0800

    fix Header enum

--- console/src/components/Common/Headers/Headers.tsx
+++ console/src/components/Common/Headers/Headers.tsx
@@ -5,7 +5,7 @@ import DropdownButton from '../DropdownButton/DropdownButton';
 import { addPlaceholderHeader } from './utils';
 
 export type Header = {
-  type: 'static' | 'env';
+  type: 'static' | 'env' | 'header';
   name: string;
   value: string;
 };
